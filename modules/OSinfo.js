45265f8a1ad024916744f90c8e2360125277b8bc
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-02-18 21:03:02 +0100

    17.5 v1

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var changeTime = require('../modules/changeTime');
 function getOSinfo() {
     var type = os.type();
@@ -7,16 +8,16 @@ function getOSinfo() {
     } else if (type === 'Windows_NT') {
         type = 'Windows';
     }
-    console.log('System:', type);
+    console.log(('System:').magenta, type);
     var release = os.release();
-    console.log('Release:', release);
+    console.log(('Release:').green, release);
     var cpu = os.cpus()[0].model;
-    console.log('CPU model:', cpu);
+    console.log(('CPU model:').yellow, cpu);
     var uptime = os.uptime()
-    console.log('Uptime: ' + (changeTime.print(uptime)));
+    console.log(('Uptime: ').cyan + (changeTime.print(uptime)));
     var userInfo = os.userInfo();
-    console.log('User name:' + userInfo.username);
-    console.log('Home dir:' + userInfo.homedir);
+    console.log(('User name:').rainbow + userInfo.username);
+    console.log(('Home dir:').red + userInfo.homedir);
 };
 exports.print = getOSinfo;
 
